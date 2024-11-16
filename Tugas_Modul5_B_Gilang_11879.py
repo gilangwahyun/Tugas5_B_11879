da1f8fa1a51386896207761095651b8852ac6d25
Gilang Wahyu Nugraha <220711879@students.uajy.ac.id>
2024-11-16 17:41:40 +0700

    Update Tugas_Modul5_B_Gilang_11879.py

--- Tugas_Modul5_B_Gilang_11879.py
+++ Tugas_Modul5_B_Gilang_11879.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import os
 
-model_path = r'C:\Users\lenovo\OneDrive\Documents\VSCODE\Python\Tugas5_B_11879\best_model_tf.h5'
+model_path = r'best_model_tf.h5'
 
 if os.path.exists(model_path):
     try:
